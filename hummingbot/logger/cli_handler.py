373139b7eba39046480e6ed67b8cb9080137515c
Luke Grimstrup <luke@enform.io>
2024-08-16 16:57:37 +0300

    added milliseconds to cli_handler

--- hummingbot/logger/cli_handler.py
+++ hummingbot/logger/cli_handler.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
+from datetime import datetime
 from logging import StreamHandler
 from typing import Optional
-from datetime import datetime
 
 
 class CLIHandler(StreamHandler):
@@ -13,7 +13,7 @@ class CLIHandler(StreamHandler):
         exc_info = record.exc_info
         if record.exc_info is not None:
             record.exc_info = None
-        retval = f'{datetime.fromtimestamp(record.created).strftime("%H:%M:%S")} - {record.name.split(".")[-1]} - ' \
+        retval = f'{datetime.fromtimestamp(record.created).strftime("%H:%M:%S.%f")[:-3]} - {record.name.split(".")[-1]} - ' \
                  f'{record.getMessage()}'
         if exc_info:
             retval += " (See log file for stack trace dump)"
